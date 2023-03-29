842fec6219705e728fb125414d051493611bebe0
Diletta Pelloni <dilpel@live.it>
2023-03-29 15:40:06 +0200

    Fatte milestone 1 e 2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Models
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        $projects = Project::with('type', 'technologies')->get();
+
+        return response()->json([
+            'success'=> true,
+            'code'=> 200,
+            'message'=> 'Ok',
+            'projects'=> $projects
+        ]);
+    }
+}
